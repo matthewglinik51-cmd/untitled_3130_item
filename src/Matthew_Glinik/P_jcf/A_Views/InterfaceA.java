9d38b3bae8be504b8cf20a9ac22615db0d3ce578
matthewglinik51-cmd <matthew.glinik51@gmail.com>
2025-12-07 08:10:05 -0500

    Dec7Sun

--- src/Matthew_Glinik/P_jcf/A_Views/InterfaceA.java
+++ src/Matthew_Glinik/P_jcf/A_Views/InterfaceA.java
@@ -2,5 +2,5 @@ package Matthew_Glinik.P_jcf.A_Views;
 
 import Matthew_Glinik.F_generics.Matthew_Glinik_Tool_Kit;
 
-public class InterfaceA<E> implements Matthew_Glinik_Tool_Kit<E> {
+public interface InterfaceA<E> extends Matthew_Glinik_Tool_Kit<E> {
 }
