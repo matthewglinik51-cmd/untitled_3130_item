e0ebb0a537e1a5afb4bbbc50ad70787d44c37adb
matthewglinik51-cmd <matthew.glinik51@gmail.com>
2025-12-08 13:43:17 -0500

    Dec8Mon

--- src/Matthew_Glinik/P_jcf/A_Views/InterfaceA.java
+++ src/Matthew_Glinik/P_jcf/A_Views/InterfaceA.java
@@ -86,13 +86,61 @@ public interface InterfaceA<E> extends Matthew_Glinik_Tool_Kit<E> {
 
             public InterfaceA_unmodifiable_VIEW(InterfaceA<E> original)
             {
-                // this.original = UnmodifiableImplF_A<E>(original);
-                this.original = original;
+                this.original = UnmodifiableImplF_A<E>(original);
+                // this.original = original;
             }
 
             public InterfaceA<E> original_view()
             {
                 return new InterfaceA_original_VIEW(this.original);
+            }
+
+            @Override
+            public E getFirst() {
+                String errorMessage = "did not specify \"unmodifiable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            @Override
+            public E getLast() {
+                String errorMessage = "did not specify \"unmodifiable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            @Override
+            public boolean addFirst(E given_value_to_add) {
+                String errorMessage = "did not specify \"unmodifiable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            @Override
+            public E removeLast() {
+                String errorMessage = "did not specify \"unmodifiable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            @Override
+            public E removeFirst() {
+                String errorMessage = "did not specify \"unmodifiable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            @Override
+            public boolean addLast(E given_value_to_add) {
+                String errorMessage = "did not specify \"unmodifiable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            @Override
+            public E setFirst(E given_value_to_set) {
+                String errorMessage = "did not specify \"unmodifiable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            @Override
+            public E setLast(E given_value_to_set) {
+                String errorMessage = "did not specify \"unmodifiable_view()\" type. "
+                throw new Exception(errorMessage);
             }
 
             private class InterfaceA_original_VIEW<E> implements InterfaceA<E>
@@ -207,7 +255,173 @@ public interface InterfaceA<E> extends Matthew_Glinik_Tool_Kit<E> {
         }
 
         private class InterfaceA_modifiable_VIEW<E> implements InterfaceA<E>
-        {}
+        {
+            private InterfaceA<E> original;
+
+            public InterfaceA_unmodifiable_VIEW(InterfaceA<E> original)
+            {
+                this.original = UnmodifiableImplF_A<E>(original);
+                // this.original = original;
+            }
+
+            public InterfaceA<E> original_view()
+            {
+                return new InterfaceA_original_VIEW(this.original);
+            }
+
+            @Override
+            public E getFirst() {
+                String errorMessage = "did not specify \"modifiable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            @Override
+            public E getLast() {
+                String errorMessage = "did not specify \"modifiable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            @Override
+            public boolean addFirst(E given_value_to_add) {
+                String errorMessage = "did not specify \"modifiable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            @Override
+            public E removeLast() {
+                String errorMessage = "did not specify \"modifiable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            @Override
+            public E removeFirst() {
+                String errorMessage = "did not specify \"modifiable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            @Override
+            public boolean addLast(E given_value_to_add) {
+                String errorMessage = "did not specify \"modifiable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            @Override
+            public E setFirst(E given_value_to_set) {
+                String errorMessage = "did not specify \"modifiable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            @Override
+            public E setLast(E given_value_to_set) {
+                String errorMessage = "did not specify \"modifiable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            private class InterfaceA_original_VIEW<E> implements InterfaceA<E>
+            {
+                private InterfaceA<E> original;
+
+                public InterfaceA_original_VIEW(InterfaceA<E> original)
+                {
+                    this.original = original;
+                }
+
+                @Override
+                public E getFirst() {
+                    return this.original.getFirst();
+                }
+
+                @Override
+                public E getLast() {
+                    return this.original.getLast();
+                }
+
+                @Override
+                public boolean addFirst(E given_value_to_add) {
+                    return this.original.addFirst(given_value_to_add);
+                }
+
+                @Override
+                public E removeLast() {
+                    return this.original.removeLast();
+                }
+
+                @Override
+                public E removeFirst() {
+                    return this.original.removeFirst();
+                }
+
+                @Override
+                public boolean addLast(E given_value_to_add) {
+                    return this.original.addLast(given_value_to_add);
+                }
+
+                @Override
+                public E setFirst(E given_value_to_set) {
+                    return this.original.setFirst(given_value_to_set);
+                }
+
+                @Override
+                public E setLast(E given_value_to_set) {
+                    return this.original.setLast(given_value_to_set);
+                }
+            }
+
+            public InterfaceA<E> reversed_view()
+            {
+                return new InterfaceA_reversed_VIEW(this.original);
+            }
+
+            private class InterfaceA_reversed_VIEW<E> implements InterfaceA<E>
+            {
+                private InterfaceA<E> original;
+
+                public InterfaceA_reversed_VIEW(InterfaceA<E> original)
+                {
+                    this.original = original;
+                }
+
+                @Override
+                public E getFirst() {
+                    return this.original.getLast();
+                }
+
+                @Override
+                public E getLast() {
+                    return this.original.getFirst();
+                }
+
+                @Override
+                public boolean addFirst(E given_value_to_add) {
+                    return this.original.addLast(given_value_to_add);
+                }
+
+                @Override
+                public E removeLast() {
+                    return this.original.removeFirst();
+                }
+
+                @Override
+                public E removeFirst() {
+                    return this.original.removeLast();
+                }
+
+                @Override
+                public boolean addLast(E given_value_to_add) {
+                    return this.original.addFirst(given_value_to_add);
+                }
+
+                @Override
+                public E setFirst(E given_value_to_set) {
+                    return this.original.setLast(given_value_to_set);
+                }
+
+                @Override
+                public E setLast(E given_value_to_set) {
+                    return this.original.setFirst(given_value_to_set);
+                }
+            }
+        }
 
         public InterfaceA<E> resizeable_view()
         {
@@ -215,7 +429,173 @@ public interface InterfaceA<E> extends Matthew_Glinik_Tool_Kit<E> {
         }
 
         private class InterfaceA_resizeable_VIEW<E> implements InterfaceA<E>
-        {}
+        {
+            private InterfaceA<E> original;
+
+            public InterfaceA_resizeable_VIEW(InterfaceA<E> original)
+            {
+                this.original = ResizeableImplF_A<E>(original);
+                // this.original = original;
+            }
+
+            public InterfaceA<E> original_view()
+            {
+                return new InterfaceA_original_VIEW(this.original);
+            }
+
+            @Override
+            public E getFirst() {
+                String errorMessage = "did not specify \"resizeable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            @Override
+            public E getLast() {
+                String errorMessage = "did not specify \"resizeable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            @Override
+            public boolean addFirst(E given_value_to_add) {
+                String errorMessage = "did not specify \"resizeable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            @Override
+            public E removeLast() {
+                String errorMessage = "did not specify \"resizeable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            @Override
+            public E removeFirst() {
+                String errorMessage = "did not specify \"resizeable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            @Override
+            public boolean addLast(E given_value_to_add) {
+                String errorMessage = "did not specify \"resizeable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            @Override
+            public E setFirst(E given_value_to_set) {
+                String errorMessage = "did not specify \"resizeable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            @Override
+            public E setLast(E given_value_to_set) {
+                String errorMessage = "did not specify \"resizeable_view()\" type. "
+                throw new Exception(errorMessage);
+            }
+
+            private class InterfaceA_original_VIEW<E> implements InterfaceA<E>
+            {
+                private InterfaceA<E> original;
+
+                public InterfaceA_original_VIEW(InterfaceA<E> original)
+                {
+                    this.original = original;
+                }
+
+                @Override
+                public E getFirst() {
+                    return this.original.getFirst();
+                }
+
+                @Override
+                public E getLast() {
+                    return this.original.getLast();
+                }
+
+                @Override
+                public boolean addFirst(E given_value_to_add) {
+                    return this.original.addFirst(given_value_to_add);
+                }
+
+                @Override
+                public E removeLast() {
+                    return this.original.removeLast();
+                }
+
+                @Override
+                public E removeFirst() {
+                    return this.original.removeFirst();
+                }
+
+                @Override
+                public boolean addLast(E given_value_to_add) {
+                    return this.original.addLast(given_value_to_add);
+                }
+
+                @Override
+                public E setFirst(E given_value_to_set) {
+                    return this.original.setFirst(given_value_to_set);
+                }
+
+                @Override
+                public E setLast(E given_value_to_set) {
+                    return this.original.setLast(given_value_to_set);
+                }
+            }
+
+            public InterfaceA<E> reversed_view()
+            {
+                return new InterfaceA_reversed_VIEW(this.original);
+            }
+
+            private class InterfaceA_reversed_VIEW<E> implements InterfaceA<E>
+            {
+                private InterfaceA<E> original;
+
+                public InterfaceA_reversed_VIEW(InterfaceA<E> original)
+                {
+                    this.original = original;
+                }
+
+                @Override
+                public E getFirst() {
+                    return this.original.getLast();
+                }
+
+                @Override
+                public E getLast() {
+                    return this.original.getFirst();
+                }
+
+                @Override
+                public boolean addFirst(E given_value_to_add) {
+                    return this.original.addLast(given_value_to_add);
+                }
+
+                @Override
+                public E removeLast() {
+                    return this.original.removeFirst();
+                }
+
+                @Override
+                public E removeFirst() {
+                    return this.original.removeLast();
+                }
+
+                @Override
+                public boolean addLast(E given_value_to_add) {
+                    return this.original.addFirst(given_value_to_add);
+                }
+
+                @Override
+                public E setFirst(E given_value_to_set) {
+                    return this.original.setLast(given_value_to_set);
+                }
+
+                @Override
+                public E setLast(E given_value_to_set) {
+                    return this.original.setFirst(given_value_to_set);
+                }
+            }
+        }
     }
     */
 }
