2870c9ac3d412c07ed9b791f36a48d91a5cac1b1
matthewglinik51-cmd <matthew.glinik51@gmail.com>
2025-12-16 16:32:30 -0500

    Dec16Tue

--- src/Matthew_Glinik/P_jcf/A_Views/InterfaceA_Demo.java
+++ src/Matthew_Glinik/P_jcf/A_Views/InterfaceA_Demo.java
@@ -1,12 +1,14 @@
 package Matthew_Glinik.P_jcf.A_Views;
 
 public class InterfaceA_Demo {
-    static void main()
+    public static void main(String[] args)
     {
         InterfaceA<Integer> InterfaceA_E = new InterfaceA_Impl<>();
         InterfaceA_E.setLast(10);
         InterfaceA_E.setFirst(5);
         InterfaceA_E.addLast(20);
         InterfaceA_E.addFirst(23);
+        InterfaceA<Integer> InterfaceA_E_VIEW = InterfaceA_E.view();
+        InterfaceA<Integer> InterfaceA_E_VIEW_unmodifiable = InterfaceA_E.view().unmodifiable_view();
     }
 }
